a9a19a718d843e49f15d85c99ff0a27e126d80c2
Noriko <norko.misra@ischool.berkeley.edu>
2015-04-04 19:32:58 -0700

    fixed errors.

--- others/create_lang_region.py
+++ others/create_lang_region.py
@@ -9,7 +9,8 @@ for row in reader:
 	if row_items[0] not in country_list:
 		country_list.append(row_items[0])
 del country_list[0]
-print(country_list)
+# print(country_list)
+
 
 #create lang list
 f_lang = open('lang.csv', 'rU')
@@ -27,10 +28,13 @@ del lang_country_list[0]
 for items in country_list:
 	if items not in lang_country_list:
 		missing_lang_list.append(items)
-print(lang_list)
-print(len(lang_list))
-print(lang_country_list)
-print(missing_lang_list)
+# print(lang_list)
+# print(lang_country_list)
+# print(missing_lang_list)
+# print(len(country_list))
+# print(len(lang_list))
+# print(len(missing_lang_list))
+
 
 out = open('data_lang.txt', 'w')
 out.write("country,lang\n")
@@ -55,24 +59,39 @@ for row in reader_region:
 
 del region_country_list[0]
 for items in country_list:
-	if items not in lang_country_list:
+	if items not in region_country_list:
 		missing_region_list.append(items)
-print(lang_list)
-print(len(lang_list))
-print(lang_country_list)
-print(missing_lang_list)
+# print(region_list)
+# print(len(region_list))
+# print(region_country_list)
+# print(missing_region_list)
+
 
 
 out = open('data_region.txt', 'w')
 out.write("country,region\n")
 for items in region_list:
 	out.write(items[0] +',' + items[1] + "\n")
-for items in missing_lang_list:
+for items in missing_region_list:
 	out.write(items+','+'\n')
+
 
 f.close()
 f_lang.close()
-#f_region.close()
+f_region.close()
 out.close()
 
+#debug
+# f = open('data_region.txt', 'r')
+# counter = 0
+# reader = f.readlines()
+# for items in reader:
+#  counter +=1
+# print(counter)
 
+# f = open('data_lang.txt', 'r')
+# counter = 0
+# reader = f.readlines()
+# for items in reader:
+#  counter +=1
+# print(counter)
