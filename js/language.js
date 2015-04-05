31ebcf9e0344628cb5aff7d7a17a12712b5cfb10
yusuzuki-ucb <yusuzuki@ischool.berkeley.edu>
2015-04-05 12:40:46 -0700

    Revert "Deleted unnecessary codes"
    This reverts commit e6d78ea2f6c59efda0875ad10fc13b4883cac773.

--- js/language.js
+++ js/language.js
@@ -14,3 +14,17 @@ d3.csv('data/data_lang.txt', function (error, data) {
         
     });
 });
+
+// Returns languages of a chord by string
+function getLanguages(d) {
+    var s = rdr(d).sname;
+    var t = rdr(d).tname;
+    var lang = _.intersection(lang_dictionary[s], lang_dictionary[t]);
+    //
+    var result = "";
+    _.each(lang, function (e) {
+        result += e + " ";
+    });
+    result = result.trim();
+    return result;
+}
